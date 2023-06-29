64a2a0ae50fa5055ff105bca66a0f63454543f0c
unknown <kunashaari@gmail.com>
2023-06-30 02:39:54 +0530

    resolved

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import {Outlet} from 'react-router-dom'
 import './App.css'
 
-import Home from './components/Home'
 import Nav from './parts/Nav'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
